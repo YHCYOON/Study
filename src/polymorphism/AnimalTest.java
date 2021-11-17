20c72d4e8e4dee137046bef6cdfacc7d2c012381
YHCYOON <yhc920923@gmail.com>
2021-11-17 17:00:00 +0900

    polymophism

--- src/polymorphism/AnimalTest.java
+++ src/polymorphism/AnimalTest.java
@@ -14,6 +14,9 @@ class Human extends Animal{
 	public void move() {
 		System.out.println("사람이 두발로 걷습니다");
 	}
+	public void readBooks() {
+		System.out.println("사람이 책을 읽습니다");
+	}
 }
 
 class Tiger extends Animal{
@@ -21,11 +24,18 @@ class Tiger extends Animal{
 	public void move() {
 		System.out.println("호랑이가 네 발로 뜁니다");
 	}
+	
+	public void hunting() {
+		System.out.println("호랑이가 사냥을 합니다");
+	}
 }
 
 class Eagle extends Animal{
 	public void move() {
 		System.out.println("독수리가 하늘을 날아갑니다");
+	}
+	public void flying() {
+		System.out.println("독수리가 날개를 쭉 펴고 멀리 날아갑니다");
 	}
 }
 
@@ -36,6 +46,16 @@ public class AnimalTest {
 		Animal hAnimal = new Human();
 		Animal tAnimal = new Tiger();
 		Animal eAnimal = new Eagle();
+		
+		/*
+		Human human = (Human)hAnimal;
+		human.readBooks();
+		*/
+		
+		if ( hAnimal instanceof Human) {
+			Human human = (Human)hAnimal;
+			human.readBooks();
+		}
 		
 		/*
 		AnimalTest test = new AnimalTest();
@@ -52,12 +72,37 @@ public class AnimalTest {
 		for(Animal animal : animalList) {
 			animal.move();
 		}
+		
+		AnimalTest test = new AnimalTest();
+		test.testDownCasting(animalList);
 	}
 	
 	public void moveAnimal(Animal animal) {
 		animal.move();
 	}
 	
+	public void testDownCasting(ArrayList<Animal> list) {
+		for(int i = 0; i<list.size(); i++) {
+			Animal animal = list.get(i);
+			
+			if( animal instanceof Human) {
+				Human human = (Human)animal;
+				human.readBooks();
+			}
+			else if( animal instanceof Tiger) {
+				Tiger tiger = (Tiger)animal;
+				tiger.hunting();
+			}
+			else if( animal instanceof Eagle) {
+				Eagle eagle = (Eagle)animal;
+				eagle.flying();
+			}
+			else {
+				System.out.println("error");
+			}
+		
+		}
+	}
 	
 	
 	
