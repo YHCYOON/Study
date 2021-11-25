5a0ecd959ee784269f33346d442ab1ae7f3b8cbc
YHCYOON <yhc920923@gmail.com>
2021-11-25 17:28:02 +0900

    generic

---
+++ src/generic/Water.java
@@ -0,0 +1,14 @@
+package generic;
+
+public class Water extends Material{
+
+	public String toString() {
+		return "재료는 Water 입니다";
+	}
+
+	@Override
+	public void doPrinting() {
+		// TODO Auto-generated method stub
+		
+	}
+}
