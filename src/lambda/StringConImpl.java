2b5a15d3034e6ce17ecccf2a07a8dbb4a7b6ea0f
YHCYOON <yhc920923@gmail.com>
2021-11-30 16:20:27 +0900

    lambda

---
+++ src/lambda/StringConImpl.java
@@ -0,0 +1,15 @@
+package lambda;
+
+public class StringConImpl implements StringConcat{
+
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void makeString(String s1, String s2) {
+
+		System.out.println(s1 + "," + s2);
+	}
+
+}
