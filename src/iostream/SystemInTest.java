a15018f5d0939922825713bf4cebb2fe2b6a4a98
YHCYOON <yhc920923@gmail.com>
2021-12-01 18:14:16 +0900

    iostream

---
+++ src/iostream/SystemInTest.java
@@ -0,0 +1,22 @@
+package iostream;
+
+import java.io.IOException;
+
+public class SystemInTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("ют╥б : ");
+		
+		try {
+			int i = System.in.read();
+			System.out.println(i);
+			System.out.println((char)i);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		
+	}
+
+}
